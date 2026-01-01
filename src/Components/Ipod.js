e44c8adaf95e1e9f724b4aceae4fd0cd2c91db35
Mantur <tanmay.mantur.ext@siemens.com>
2026-01-01 18:47:34 +0530

    Refactored

--- src/Components/Ipod.js
+++ src/Components/Ipod.js
@@ -3,18 +3,10 @@ import styles from "./Ipod.module.css"
 import WheelIcon from "./WheelIcon";
 import MenuScreen from "./MenuScreen";
 
-const mainMenu = [
-    "Cover Flow",
-    "Music",
-    "Game",
-    "Settings"
-]
-
-const musicMenu = [
-    'All Songs',
-    'Artists',
-    'Albums'
-]
+const MENUS = {
+    mainMenu: ["Cover Flow", "Music", "Game", "Settings"],
+    musicMenu: ["All Songs", "Artists", "Albums"]
+};
 
 export default class Ipod extends Component {
     constructor() {
@@ -22,7 +14,7 @@ export default class Ipod extends Component {
         this.state = {
             activeIndex: 0,
             screenContent: 'mainMenu',
-            currentMenu: mainMenu
+            currentMenu: MENUS.mainMenu
         }
         this.wheelRef = createRef();
         this.lastAngle = null;
@@ -36,8 +28,7 @@ export default class Ipod extends Component {
         const x = clientX - centerX;
         const y = clientY - centerY;
         let degrees = Math.atan2(y, x) * (180 / Math.PI);
-        if (degrees < 0) degrees += 360;
-        return degrees;
+        return degrees < 0 ? degrees + 360 : degrees;
     }
 
     handleMouseUp = () => {
@@ -54,8 +45,7 @@ export default class Ipod extends Component {
             if (delta < -180) delta += 360;
 
             if (Math.abs(delta) >= this.sensitivity) {
-                const direction = delta > 0 ? 1 : -1;
-                this.updateIndex(direction);
+                this.updateIndex(delta > 0 ? 1 : -1);
                 this.lastAngle = currentAngle;
             }
         }
@@ -79,47 +69,38 @@ export default class Ipod extends Component {
         })
     }
 
-    handleSelectMenu = (activeIndex) => {
-        const { currentMenu } = this.state;
-        if (activeIndex === 'mainMenu') {
+    handleSelectMenu = (e) => {
+        e.stopPropagation();
+        const { activeIndex, currentMenu } = this.state;
+        const selectedItem = currentMenu[activeIndex];
+
+        if (selectedItem === "Music") {
             this.setState({
-                screenContent: "mainMenu"
-            })
-        }
-        if (activeIndex === 0) {
+                currentMenu: MENUS.musicMenu,
+                screenContent: "Music",
+                activeIndex: 0
+            });
+        } else {
             this.setState({
-                screenContent: currentMenu[activeIndex]
-            })
-        }
-        if (activeIndex === 1) {
-            this.setState({
-                currentMenu: musicMenu,
-                screenContent: currentMenu[activeIndex]
-            })
-        }
-        if (activeIndex === 2) {
-            this.setState({
-                screenContent: currentMenu[activeIndex]
-            })
-        }
-        if (activeIndex === 3) {
-            this.setState({
-                screenContent: currentMenu[activeIndex]
-            })
+                screenContent: selectedItem
+            });
         }
     }
 
     handleMenuClick = () => {
         const { screenContent } = this.state;
-        if (musicMenu.includes(screenContent)) {
+        if (MENUS.musicMenu.includes(screenContent)) {
             this.setState({
-                screenContent: "Music"
+                screenContent: "Music",
+                currentMenu: MENUS.musicMenu,
+                activeIndex: 0
             })
         }
         else {
             this.setState({
                 screenContent: "mainMenu",
-                currentMenu: mainMenu
+                currentMenu: MENUS.mainMenu,
+                activeIndex: 0
             })
         }
     }
@@ -127,49 +108,49 @@ export default class Ipod extends Component {
     renderContent = (screenContent, activeIndex) => {
         switch (screenContent) {
             case "mainMenu":
-                return <MenuScreen title="Ipod.js" activeIndex={activeIndex} menuItems={mainMenu} />;
-            case "Cover Flow":
                 return (
-                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                        <h3>Cover Flow</h3>
-                    </div>
+                    <MenuScreen
+                        title="Ipod.js"
+                        activeIndex={activeIndex}
+                        menuItems={MENUS.mainMenu}
+                    />
+                );
+
+            case "Music":
+                return (
+                    <MenuScreen
+                        title="Music"
+                        activeIndex={activeIndex}
+                        menuItems={MENUS.musicMenu}
+                    />
                 );
             case "Game":
                 return (
-                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                        <img height='75' src="https://cdn-icons-png.flaticon.com/128/686/686589.png" alt="games" />
+                    <div className={styles.center}>
+                        <img height="75" src="https://cdn-icons-png.flaticon.com/128/686/686589.png" />
                         <h3>Games</h3>
                     </div>
                 );
+
             case "Settings":
                 return (
-                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                        <img height='75' src="https://cdn-icons-png.flaticon.com/128/2040/2040504.png" alt="setting" />
+                    <div className={styles.center}>
+                        <img height="75" src="https://cdn-icons-png.flaticon.com/128/2040/2040504.png" />
                         <h3>Settings</h3>
                     </div>
                 );
-            case "Music":
-                return <MenuScreen title="Music" activeIndex={activeIndex} menuItems={musicMenu} />;
-            case 'All Songs':
+            case "Cover Flow":
+            case "All Songs":
+            case "Artists":
+            case "Albums":
                 return (
-                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                        <h3>All songs List</h3>
+                    <div className={styles.center}>
+                        <h3>{screenContent}</h3>
                     </div>
                 );
-            case 'Artists':
-                return (
-                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                        <h3>All Artists</h3>
-                    </div>
-                );
-            case 'Albums':
-                return (
-                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                        <h3>All Albums</h3>
-                    </div>
-                );
+
             default:
-                return <h1>Something Went Wrong</h1>
+                return <h3>Something went wrong</h3>;
         }
     }
 
@@ -186,23 +167,32 @@ export default class Ipod extends Component {
                         className={styles.outerWheel}
                         onMouseMove={this.handleMouseMove}
                     >
-                        <span onClick={this.handleMenuClick} style={{ position: 'absolute', top: 15, color: 'grey', fontWeight: 'bold', cursor: 'default' }}>Menu</span>
-                        <span style={{ position: 'absolute', bottom: 5 }}><WheelIcon
+                        <span
+                            onClick={this.handleMenuClick}
+                            className={styles.menuButton}>
+                            Menu
+                        </span>
+                        <span className={styles.playButton}><WheelIcon
                             src="https://cdn-icons-png.flaticon.com/128/8428/8428588.png"
                             alt="play/pause"
                             iconHeight={30}
                         /></span>
-                        <span style={{ position: 'absolute', left: 15 }}><WheelIcon
+                        <span className={styles.prevButton}><WheelIcon
                             src="https://cdn-icons-png.flaticon.com/128/254/254437.png"
                             alt="previous"
                         /></span>
-                        <span style={{ position: 'absolute', right: 15 }}>
+                        <span className={styles.nextButton}>
                             <WheelIcon
                                 src="https://cdn-icons-png.flaticon.com/128/3567/3567831.png"
                                 alt="next"
                             />
                         </span>
-                        <span onClick={() => this.handleSelectMenu(activeIndex)} className={styles.innerWheel}></span>
+                        <span
+                            onMouseMove={(e) => {
+                                e.stopPropagation();
+                            }}
+                            onClick={this.handleSelectMenu}
+                            className={styles.innerWheel}></span>
                     </div>
                 </div>
             </div>
